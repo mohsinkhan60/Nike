e56ff3fa8708ba6634765ef63154c7d3532c09d1
mohsinkhan60 <mohsinsaffy@gmail.com>
2024-08-19 13:03:13 +0500

    add superQuality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,8 @@ export const Button = ({ label, iconURL }) => {
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full border-coral-red">
       {/* click me */}
       {label}
-      <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+      {iconURL && 
+      <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />}
     </button>
   );
 };
