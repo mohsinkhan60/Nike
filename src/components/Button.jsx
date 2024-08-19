ca5270ae0897b0b8a3ca4427fadb085e072b5673
mohsinkhan60 <mohsinsaffy@gmail.com>
2024-08-19 19:22:32 +0500

    add subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,24 @@
-export const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+export const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : `bg-coral-red border-coral-red`} rounded-full`}>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${
+        backgroundColor
+          ? `${backgroundColor} ${borderColor} ${textColor}`
+          : `bg-coral-red border-coral-red`
+      } rounded-full ${fullWidth ? "w-full" : ""}`}
+    >
       {/* click me */}
       {label}
-      {iconURL && 
-      <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />}
+      {iconURL && (
+        <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+      )}
     </button>
   );
 };
